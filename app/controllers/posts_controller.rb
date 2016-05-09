bfe8dbb9d062216a33ea420f665cff4cba273e12
Devin Miller <edwin0258@gmail.com>
2016-05-09 23:38:43 +0000

    Revise index page design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,9 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
     end
+    def show
+        @post = Post.find(params[:id])
+    end
     def create
         @post = Post.new(post_params)
         
